526bb7e6e3b9b6b003c2b703edf1b3d57981a7ad
Lianny Coringa <lianny_costa@hotmail.com>
2023-04-26 17:44:07 -0300

    Requisitos 16 ao 19

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ const email = document.querySelector('#email');
 const senha = document.querySelector('#senha');
 const buttonEntrar = document.querySelector('#buttonEntrar');
 buttonEntrar.addEventListener('click', () => {
-    if (email.value === 'tryber@teste.com' && senha.value === '123456') {
-        alert('Olá, Tryber!');
-    } else {
-        alert('Email ou senha inválidos.');
-    }
-})
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
+
+const buttonSubmit = document.querySelector('#submit-btn');
+const checkbox = document.querySelector('#agreement');
+buttonSubmit.disabled = true;
+checkbox.addEventListener('click', () => {
+  buttonSubmit.disabled = false;
+});
